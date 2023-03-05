ce798c41546ca186ba23d8ea23026e5394cf04cf
jorismertz <35079666+jorismertz@users.noreply.github.com>
2023-03-05 22:16:06 +0100

    remove redundant killall cmd

--- install/mac-defaults.sh
+++ install/mac-defaults.sh
@@ -6,7 +6,6 @@ defaults write com.apple.dock "orientation" -string "right"
 defaults write com.apple.dock "autohide" -bool "true"
 defaults write com.apple.dock autohide-time-modifier -int 0
 defaults write com.apple.dock autohide-delay -float 0
-killall Dock
 
 # Finder
 defaults write com.apple.finder "AppleShowAllFiles" -bool "true"
@@ -20,6 +19,7 @@ defaults write com.apple.finder "ShowExternalHardDrivesOnDesktop" -bool "false"
 
 # Mission control
 defaults write com.apple.dock "mru-spaces" -bool "false"
+
 killall Dock
 
 # Disable the sound effects on boot
